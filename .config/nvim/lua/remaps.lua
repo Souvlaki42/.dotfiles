4add155baa425ef38ebb9d9195b49e6ce84c4764
Souvlaki42 <77171255+Souvlaki42@users.noreply.github.com>
2024-02-18 17:34:13 +0200

    Nvim keymaps for clipboard fixed

--- .config/nvim/lua/remaps.lua
+++ .config/nvim/lua/remaps.lua
@@ -8,30 +8,29 @@ map("n", "<leader>pv", vim.cmd.Ex)
 map({ "n", "v" }, "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Make script executable
-map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make executable" })
-
--- Quick save file(s)
-
-map("n", "<leader>s", ":w<CR>")
-map("n", "<leader>S", ":wa<CR>")
+map("n", "<leader>ex", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make executable" })
 
 -- Move line(s) up and down
-map("n", "<A-j>", ":m .-2<CR>==", {desc="Move line up"})
-map("n", "<A-k>", ":m .+1<CR>==", {desc="Move line down"})
-map("v", "<A-j>", ":m '<-2<CR>gv=gv", {desc="Move line up"})
-map("v", "<A-k>", ":m '>+1<CR>gv=gv", {desc="Move line down"})
+map("n", "<A-j>", ":m .-2<CR>==", { desc = "Move line up" })
+map("n", "<A-k>", ":m .+1<CR>==", { desc = "Move line down" })
+map("v", "<A-j>", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+map("v", "<A-k>", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 
 -- Yank/Copy into system clipboard
-map({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank motion" })
-map({ "n", "v" }, "<leader>Y", '"+Y', { desc = "Yank line" })
+map("n", "<C-c>", '"+Y', { desc = "Yank line" })
+map("v", "<C-c>", '"+y', { desc = "Yank motion" })
 
 -- Delete/Cut into system clipboard
-map({ "n", "v" }, "<leader>d", '"+d', { desc = "Delete motion" })
-map({ "n", "v" }, "<leader>D", '"+D', { desc = "Delete line" })
+map("n", "<C-x>", '"+D', { desc = "Delete line" })
+map("v", "<C-x>", '"+d', { desc = "Delete motion" })
 
 -- Paste from system clipboard
-map("n", "<leader>p", '"+p', { desc = "Paste after cursor" })
-map("n", "<leader>P", '"+P', { desc = "Paste before cursor" })
+map("n", "<C-v>", '"+P', { desc = "Paste line" })
+map("v", "<C-v>", '"+p', { desc = "Paste motion" })
 
--- normal mode u to undo
--- normal mode <C-r> to redo
+-- Undo and redo
+map("n", "u", ":undo<CR>", { desc = "Undo" })
+map("n", "r", ":redo<CR>", { desc = "Redo" })
+
+-- Select all
+map("n", "<C-a>", "ggVG", { noremap = true })
