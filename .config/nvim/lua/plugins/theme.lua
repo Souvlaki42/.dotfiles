3c79c15fb86f926c6e460445d6f30185f638ffbc
Souvlaki42 <souvlaki420@gmail.com>
2024-12-07 01:24:52 +0200

    feat: noice nvim macro config

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,19 +1,34 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function ()
-      require("tokyonight").setup({
-        style = "night"
-      })
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+			})
 
-      vim.cmd("colorscheme tokyonight-night")
-    end
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = { options = { theme = "tokyonight" } },
-  },
+			vim.cmd("colorscheme tokyonight-night")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "tokyonight",
+				},
+				sections = {
+					lualine_x = {
+						{
+							require("noice").api.statusline.mode.get,
+							cond = require("noice").api.statusline.mode.has,
+							color = { fg = "#ff9e64" },
+						},
+					},
+				},
+			})
+		end,
+	},
 }
